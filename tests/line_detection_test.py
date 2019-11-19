cf19dc8ff4f9fd6a9bcc2f74cc03952854940817
thitch <21094910+thitchcox@users.noreply.github.com>
2019-11-19 12:38:46 -0500

    Using Shi-Tomasi corners for lane point identification

--- tests/line_detection_test.py
+++ tests/line_detection_test.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from dtProjectHarris import dtProjectHarris
+import time
 
 # DONE : 
 # - Detect harris an orb features on various images.  Harris on yellow bw mask
@@ -17,19 +18,21 @@ from dtProjectHarris import dtProjectHarris
 # - Pipe the outout of gp fit into estimation node
 
 # Select an image to process
-# src = cv2.imread('frame0003.jpg')
+src = cv2.imread('frame0003.jpg')
 # src = cv2.imread('frame0446.jpg')
 # src = cv2.imread('frame0747.jpg')
-src = cv2.imread('frame0141.jpg')
+# src = cv2.imread('frame0141.jpg')
 
 # Select an image processing choice
 # choice = "harris_gray"
-choice = "harris_only_yellow"
+# choice = "harris_only_yellow"
 # choice = "harris_only_white"
 # choice = "orb"
 # choice = "orb_gray"
 # choice = "orb_only_yellow"
 # choice = "orb_only_white"
+choice = "gf2t_gray" 
+# choice = "gf2t_only_yellow" 
 
 ################################################
 
@@ -61,6 +64,8 @@ hsv_white1 = np.array([0,0,150])
 hsv_white2 = np.array([180,60,255])
 cropped_white_mask = cv2.inRange(img_hsv, hsv_white1, hsv_white2)
 
+time_start = time.time()
+
 # TRY : Harris corners and corner locations
 if choice == "harris_gray" or choice == "harris_only_yellow" or \
     choice == "harris_only_white":
@@ -90,6 +95,43 @@ if choice == "orb" or choice == "orb_gray" or choice == "orb_only_yellow" or \
     orb_xy = np.zeros((len(kps_orb), 2))
     for lv1 in range(len(kps_orb)):  
         orb_xy[lv1, :] = [kps_orb[lv1].pt[0], kps_orb[lv1].pt[1]]
+
+# TRY : Good features to track
+if choice == "gf2t_gray" or choice == "gf2t_only_yellow" or \
+    choice == "gf2t_only_white":
+    max_corners = 50
+    quality_level = 0.01
+    min_distance = 5
+    block_size = 7
+    if choice == "gf2t_gray":
+        corner_xy = cv2.goodFeaturesToTrack(gray_cropped_smoothed, max_corners, quality_level, min_distance, mask=cropped_yellow_mask, \
+            blockSize=block_size, useHarrisDetector=0, k=0.04)
+
+    if choice == "gf2t_only_yellow":
+        corner_xy = cv2.goodFeaturesToTrack(cropped_yellow_mask, max_corners, quality_level, min_distance, blockSize=block_size, useHarrisDetector=1, k=0.04)
+
+    # Reset parameters for identifying white points
+    max_corners = 50
+    quality_level = 0.001
+    min_distance = 10
+    block_size = 15
+    if choice == "gf2t_gray":
+        corner_xy = cv2.goodFeaturesToTrack(gray_cropped_smoothed, max_corners, quality_level, min_distance, mask=cropped_white_mask, \
+            blockSize=block_size, useHarrisDetector=0, k=0.04)
+
+    # Show corners on greyscale image
+    dts = gray_cropped_smoothed.copy()
+    corner_xy = np.int0(corner_xy)
+    for lv1 in corner_xy:
+        x,y = lv1.ravel()
+        cv2.circle(dts, (x,y), 3, 0, -1)
+
+time_end = time.time()
+
+print("Execution time is", time_end - time_start)
+
+# Printing and debugging.
+print(corner_xy)
 
 # Print the image
 cv2.imwrite('output.jpg', dts)
